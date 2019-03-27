b2c75aadc86c41c289a19851c1e53816ab29d30c
Henrique de Castilhos <henriquedecastilhos@gmail.com>
2019-03-27 16:15:53 -0300

    botões e etc

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,33 @@
   <div id="app">
     <div class="container" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
       <div class="mb-3 h1 text-center" id="page-title">
-        Calculadora.js
+        Equação de Segundo Grau
+      </div>
+
+      <div class="row">
+        <div class="input-group mb-3 col">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Valor A</span>
+          </div>
+          <input v-model="valorA" type="number" class="form-control">
+        </div>
+        <div class="input-group mb-3 col">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Valor B</span>
+          </div>
+          <input v-model="valorB" type="number" class="form-control">
+        </div>
+        <div class="input-group mb-3 col">
+          <div class="input-group-prepend">
+            <span class="input-group-text">Valor C</span>
+          </div>
+          <input v-model="valorC" type="number" class="form-control">
+        </div>
+      </div>
+      <div class="row">
+        <button class="btn col btn-outline-primary mx-2">
+          Calcular
+        </button>
       </div>
     </div>
   </div>
@@ -12,14 +38,3 @@
 export default {
 }
 </script>
-
-<style>
-/* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-</style>
